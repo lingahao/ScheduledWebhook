5f70db1e12e79a24750c3f9332bfbc25781f15c9
Gahao <90499218+lingahao@users.noreply.github.com>
2025-05-19 10:37:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import os
 
 # url = os.environ["webhook_url"]
 url = 'https://9097-59-125-150-114.ngrok-free.app/callback'
-
 data = {
+    "destination": "Uc4650f10bf0cb6121b34a9ce775f6b18",
     "events": [
         {
             "type": "message",
             "replyToken": "test123",
             "source": {
-                "userId": "Uc4650f10bf0cb6121b34a9ce775f6b18",  # 改成你自己的 LINE userId
+                "userId": "Uc4650f10bf0cb6121b34a9ce775f6b18",
                 "type": "user"
             },
             "timestamp": int(time.time() * 1000),
@@ -22,10 +22,8 @@ data = {
                 "text": "讀取"
             }
         }
-    ],
-    "destination": "Uc4650f10bf0cb6121b34a9ce775f6b18"
+    ]
 }
-
 response = requests.post(url, json=data)
 
 print(f"Status: {response.status_code}")
